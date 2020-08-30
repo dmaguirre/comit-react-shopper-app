1e041978c13b03081e037a5eba1f11323cd2d76f
tupes <tupes23@gmail.com>
2020-08-30 03:58:28 -0600

    Added error handling

--- server/server.js
+++ server/server.js
@@ -3,8 +3,12 @@ const data = require("../data/items.json");
 
 const app = express();
 
-app.get("/items", (req, resp) => {
-  resp.json(data.items);
+app.get("/items", (req, res) => {
+  try {
+    res.json(data.items);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.listen(4000, () => console.log("Listening on port 4000"));
