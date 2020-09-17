24abaccaf6edfb43ca17b6d75b970ef14b5b7254
tupes <tupes23@gmail.com>
2020-09-16 23:15:01 -0600

    Refactored auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,3 +1,40 @@
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+const expressSession = require('express-session');
+
+const sessionSecret = process.env.SESSION_SECRET;
+const adminPassword = process.env.ADMIN_PASSWORD;
+
+passport.use(adminStrategy());
+passport.serializeUser((user, callback) => callback(null, user));
+passport.deserializeUser((user, callback) => callback(null, user));
+const authenticate = passport.authenticate('local');
+
+function setAuthMiddleware(app) {
+    app.use(expressSession({
+        secret: sessionSecret,
+        resave: false,
+        saveUninitialized: false,
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+}
+
+function adminStrategy() {
+    return new Strategy((username, password, callback) => {
+        const isAdmin = username === 'admin' && password === adminPassword;
+        if (isAdmin) {
+            callback(null, { username: 'admin' });
+        } else {
+            callback(null, false);
+        }
+    })
+}
+
+function login(req, res) {
+    res.json({ success: true });
+}
 
 function ensureAdmin(req, res, next) {
     const isAdmin = req.user && req.user.username === 'admin';
@@ -8,5 +45,8 @@ function ensureAdmin(req, res, next) {
 }
 
 module.exports = {
+    authenticate,
+    setAuthMiddleware,
+    login,
     ensureAdmin
 }
